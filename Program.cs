7753ba05e8ff92e0fb8eee0d2b3c7fc0f632469e
Nick <nicholas.eng0218@gmail.com>
2018-03-03 16:55:49 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MarchMadness2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("***** Sila Solutions Group March Madness *****");
+            Console.WriteLine("***** Author: Nicholas Eng               *****");
+            Console.WriteLine("***** Year: 2018                         *****");
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("**********************************************");
+        }
+    }
+}
