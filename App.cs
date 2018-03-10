0efab9f17d54e9f5c166ad1e376050d212f4ce42
Nick <nicholas.eng0218@gmail.com>
2018-03-10 18:40:39 -0500

    Comment out test code
    Comment out test code

--- App.cs
+++ App.cs
@@ -16,29 +16,48 @@ namespace MarchMadness
             Console.WriteLine("");    
         }
 
-        public void Start() {           
-            // TODO: Uncomment
-            // Console.WriteLine("Please Enter the First Team's Name:");
-            // string team1Name = Console.ReadLine();
+        public void Start() {         
+            Console.WriteLine("Please Enter the First Team's Name:");
+            string team1Name = Console.ReadLine();
 
-            // Console.WriteLine("Please Enter the Second Team's Name:");
-            // string team2Name = Console.ReadLine();
+            Console.WriteLine("Please Enter the Second Team's Name:");
+            string team2Name = Console.ReadLine();
 
-            // TODO: Remove stub code for debugging
-            //string team1Name = "North Carolina";
-            //string team2Name = "Gonzaga";
-            
-            //string team1Name = "SMU";
-            //string team2Name = "USC";
-
-            //string team1Name = "Wisconsin";
-            //string team2Name = "Virginia Tech";
-
-            // string team1Name = "Creighton";
-            // string team2Name = "Rhode Island";
-
-            string team1Name = "Northwestern";
-            string team2Name = "Vanderbilt";
+            // ********** FOR TESTING USE **********
+            // Console.WriteLine("Please Enter test case:");
+            // string team1Name = "";
+            // string team2Name = "";
+            // switch(Console.ReadLine())
+            // {
+            //     case "0":
+            //         team1Name = "North Carolina";
+            //         team2Name = "Gonzaga";
+            //         break;
+            //     case "1":
+            //         team1Name = "SMU";
+            //         team2Name = "USC";
+            //         break;
+            //     case "2":
+            //         team1Name = "Wisconsin";
+            //         team2Name = "Virginia Tech";
+            //         break;
+            //     case "3":
+            //         team1Name = "Creighton";
+            //         team2Name = "Rhode Island";
+            //         break;
+            //     case "4":
+            //         team1Name = "Northwestern";
+            //         team2Name = "Vanderbilt";
+            //         break;
+            //     case "5":
+            //         team1Name = "Michigan";
+            //         team2Name = "Oklahoma St";
+            //         break;
+            //     case "6":
+            //         team1Name = "Minnesota";
+            //         team2Name = "MTSU";
+            //         break;
+            // }
 
             var teams = new string[] {team1Name, team2Name};
 
@@ -59,7 +78,7 @@ namespace MarchMadness
             }
             else
             {
-                Console.WriteLine(teams[winnerResult]);
+                Console.WriteLine("The Winner is: " + teams[winnerResult]);
             }
 
 
