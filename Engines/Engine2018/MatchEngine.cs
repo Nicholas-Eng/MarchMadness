9a0d6f73b39c88d1576170b617fc1312319c5d4d
Nick <nicholas.eng0218@gmail.com>
2018-03-11 14:38:37 -0400

    Modifying spread calculations
    Modifying spread calculations to be more accurate

--- Engines/Engine2018/MatchEngine.cs
+++ Engines/Engine2018/MatchEngine.cs
@@ -68,7 +68,7 @@ namespace MarchMadness.Engines.Engine2018
         {
             HigherStatComparer(season, "Total Regular Season Win/Lose Ratio",
                 t1RegSeason.TotalRegularSeasonLoses > 0 ? t1RegSeason.TotalRegularSeasonWins / t1RegSeason.TotalRegularSeasonLoses : t1RegSeason.TotalRegularSeasonWins,
-                t2RegSeason.TotalRegularSeasonLoses > 0 ? t2RegSeason.TotalRegularSeasonWins / t2RegSeason.TotalRegularSeasonLoses : t2RegSeason.TotalRegularSeasonWins, 10);
+                t2RegSeason.TotalRegularSeasonLoses > 0 ? t2RegSeason.TotalRegularSeasonWins / t2RegSeason.TotalRegularSeasonLoses : t2RegSeason.TotalRegularSeasonWins, 3);
 
             CompareRegularSeasonWinStats(season, t1RegSeason, t2RegSeason);
             CompareRegularSeasonLoseStats(season, t1RegSeason, t2RegSeason);
@@ -78,40 +78,40 @@ namespace MarchMadness.Engines.Engine2018
         {
             HigherStatComparer(season, "Average Regular Season Win Field Goals Ratio",
                 t1RegSeason.AverageRegularSeasonWinFieldGoalsMade / t1RegSeason.AverageRegularSeasonWinFieldGoalsAttempted,
-                t2RegSeason.AverageRegularSeasonWinFieldGoalsMade / t2RegSeason.AverageRegularSeasonWinFieldGoalsAttempted, 10);
+                t2RegSeason.AverageRegularSeasonWinFieldGoalsMade / t2RegSeason.AverageRegularSeasonWinFieldGoalsAttempted, 1);
             HigherStatComparer(season, "Average Regular Season Win Three Pointers Ratio",
                 t1RegSeason.AverageRegularSeasonWinThreePointersMade / t1RegSeason.AverageRegularSeasonWinThreePointersAttempted,
-                t2RegSeason.AverageRegularSeasonWinThreePointersMade / t2RegSeason.AverageRegularSeasonWinThreePointersAttempted, 10);
+                t2RegSeason.AverageRegularSeasonWinThreePointersMade / t2RegSeason.AverageRegularSeasonWinThreePointersAttempted, 1);
             HigherStatComparer(season, "Average Regular Season Win Free Throws Ratio",
                 t1RegSeason.AverageRegularSeasonWinFreeThrowsMade / t1RegSeason.AverageRegularSeasonWinFreeThrowsAttempted,
-                t2RegSeason.AverageRegularSeasonWinFreeThrowsMade / t2RegSeason.AverageRegularSeasonWinFreeThrowsAttempted, 10);
-            HigherStatComparer(season, "Average Regular Season Win Offensive Rebounds", t1RegSeason.AverageRegularSeasonWinOffensiveRebounds, t2RegSeason.AverageRegularSeasonWinOffensiveRebounds, 10);
-            HigherStatComparer(season, "Average Regular Season Win Defensive Rebounds", t1RegSeason.AverageRegularSeasonWinDefensiveRebounds, t2RegSeason.AverageRegularSeasonWinDefensiveRebounds, 10);
-            HigherStatComparer(season, "Average Regular Season Win Assists", t1RegSeason.AverageRegularSeasonWinAssists, t2RegSeason.AverageRegularSeasonWinAssists, 10);
-            LowerStatComparer(season, "Average Regular Season Win Turnovers", t1RegSeason.AverageRegularSeasonWinTurnoversCommitted, t2RegSeason.AverageRegularSeasonWinTurnoversCommitted, 10);
-            HigherStatComparer(season, "Average Regular Season Win Steals", t1RegSeason.AverageRegularSeasonWinSteals, t2RegSeason.AverageRegularSeasonWinSteals, 10);
-            HigherStatComparer(season, "Average Regular Season Win Blocks", t1RegSeason.AverageRegularSeasonWinBlocks, t2RegSeason.AverageRegularSeasonWinBlocks, 10);
-            LowerStatComparer(season, "Average Regular Season Win Fouls", t1RegSeason.AverageRegularSeasonWinPersonalFoulsCommitted, t2RegSeason.AverageRegularSeasonWinPersonalFoulsCommitted, 10);
+                t2RegSeason.AverageRegularSeasonWinFreeThrowsMade / t2RegSeason.AverageRegularSeasonWinFreeThrowsAttempted, 1);
+            HigherStatComparer(season, "Average Regular Season Win Offensive Rebounds", t1RegSeason.AverageRegularSeasonWinOffensiveRebounds, t2RegSeason.AverageRegularSeasonWinOffensiveRebounds, 5);
+            HigherStatComparer(season, "Average Regular Season Win Defensive Rebounds", t1RegSeason.AverageRegularSeasonWinDefensiveRebounds, t2RegSeason.AverageRegularSeasonWinDefensiveRebounds, 5);
+            HigherStatComparer(season, "Average Regular Season Win Assists", t1RegSeason.AverageRegularSeasonWinAssists, t2RegSeason.AverageRegularSeasonWinAssists, 5);
+            LowerStatComparer(season, "Average Regular Season Win Turnovers", t1RegSeason.AverageRegularSeasonWinTurnoversCommitted, t2RegSeason.AverageRegularSeasonWinTurnoversCommitted, 5);
+            HigherStatComparer(season, "Average Regular Season Win Steals", t1RegSeason.AverageRegularSeasonWinSteals, t2RegSeason.AverageRegularSeasonWinSteals, 5);
+            HigherStatComparer(season, "Average Regular Season Win Blocks", t1RegSeason.AverageRegularSeasonWinBlocks, t2RegSeason.AverageRegularSeasonWinBlocks, 5);
+            LowerStatComparer(season, "Average Regular Season Win Fouls", t1RegSeason.AverageRegularSeasonWinPersonalFoulsCommitted, t2RegSeason.AverageRegularSeasonWinPersonalFoulsCommitted, 5);
         }
 
         private void CompareRegularSeasonLoseStats(int season, RegularSeason t1RegSeason, RegularSeason t2RegSeason)
         {
             HigherStatComparer(season, "Average Regular Season Lose Field Goals Ratio",
                 t1RegSeason.AverageRegularSeasonLoseFieldGoalsMade / t1RegSeason.AverageRegularSeasonLoseFieldGoalsAttempted,
-                t2RegSeason.AverageRegularSeasonLoseFieldGoalsMade / t2RegSeason.AverageRegularSeasonLoseFieldGoalsAttempted, 10);
+                t2RegSeason.AverageRegularSeasonLoseFieldGoalsMade / t2RegSeason.AverageRegularSeasonLoseFieldGoalsAttempted, 1);
             HigherStatComparer(season, "Average Regular Season Lose Three Pointers Ratio",
                 t1RegSeason.AverageRegularSeasonLoseThreePointersMade / t1RegSeason.AverageRegularSeasonLoseThreePointersAttempted,
-                t2RegSeason.AverageRegularSeasonLoseThreePointersMade / t2RegSeason.AverageRegularSeasonLoseThreePointersAttempted, 10);
+                t2RegSeason.AverageRegularSeasonLoseThreePointersMade / t2RegSeason.AverageRegularSeasonLoseThreePointersAttempted, 1);
             HigherStatComparer(season, "Average Regular Season Lose Free Throws Ratio",
                 t1RegSeason.AverageRegularSeasonLoseFreeThrowsMade / t1RegSeason.AverageRegularSeasonLoseFreeThrowsAttempted,
-                t2RegSeason.AverageRegularSeasonLoseFreeThrowsMade / t2RegSeason.AverageRegularSeasonLoseFreeThrowsAttempted, 10);
-            HigherStatComparer(season, "Average Regular Season Lose Offensive Rebounds", t1RegSeason.AverageRegularSeasonLoseOffensiveRebounds, t2RegSeason.AverageRegularSeasonLoseOffensiveRebounds, 10);
-            HigherStatComparer(season, "Average Regular Season Lose Defensive Rebounds", t1RegSeason.AverageRegularSeasonLoseDefensiveRebounds, t2RegSeason.AverageRegularSeasonLoseDefensiveRebounds, 10);
-            HigherStatComparer(season, "Average Regular Season Lose Assists", t1RegSeason.AverageRegularSeasonLoseAssists, t2RegSeason.AverageRegularSeasonLoseAssists, 10);
-            LowerStatComparer(season, "Average Regular Season Lose Turnovers", t1RegSeason.AverageRegularSeasonLoseTurnoversCommitted, t2RegSeason.AverageRegularSeasonLoseTurnoversCommitted, 10);
-            HigherStatComparer(season, "Average Regular Season Lose Steals", t1RegSeason.AverageRegularSeasonLoseSteals, t2RegSeason.AverageRegularSeasonLoseSteals, 10);
-            HigherStatComparer(season, "Average Regular Season Lose Blocks", t1RegSeason.AverageRegularSeasonLoseBlocks, t2RegSeason.AverageRegularSeasonLoseBlocks, 10);
-            LowerStatComparer(season, "Average Regular Season Lose Fouls", t1RegSeason.AverageRegularSeasonLosePersonalFoulsCommitted, t2RegSeason.AverageRegularSeasonLosePersonalFoulsCommitted, 10);
+                t2RegSeason.AverageRegularSeasonLoseFreeThrowsMade / t2RegSeason.AverageRegularSeasonLoseFreeThrowsAttempted, 1);
+            HigherStatComparer(season, "Average Regular Season Lose Offensive Rebounds", t1RegSeason.AverageRegularSeasonLoseOffensiveRebounds, t2RegSeason.AverageRegularSeasonLoseOffensiveRebounds, 5);
+            HigherStatComparer(season, "Average Regular Season Lose Defensive Rebounds", t1RegSeason.AverageRegularSeasonLoseDefensiveRebounds, t2RegSeason.AverageRegularSeasonLoseDefensiveRebounds, 5);
+            HigherStatComparer(season, "Average Regular Season Lose Assists", t1RegSeason.AverageRegularSeasonLoseAssists, t2RegSeason.AverageRegularSeasonLoseAssists, 5);
+            LowerStatComparer(season, "Average Regular Season Lose Turnovers", t1RegSeason.AverageRegularSeasonLoseTurnoversCommitted, t2RegSeason.AverageRegularSeasonLoseTurnoversCommitted, 5);
+            HigherStatComparer(season, "Average Regular Season Lose Steals", t1RegSeason.AverageRegularSeasonLoseSteals, t2RegSeason.AverageRegularSeasonLoseSteals, 5);
+            HigherStatComparer(season, "Average Regular Season Lose Blocks", t1RegSeason.AverageRegularSeasonLoseBlocks, t2RegSeason.AverageRegularSeasonLoseBlocks, 5);
+            LowerStatComparer(season, "Average Regular Season Lose Fouls", t1RegSeason.AverageRegularSeasonLosePersonalFoulsCommitted, t2RegSeason.AverageRegularSeasonLosePersonalFoulsCommitted, 5);
         }
 
         private void CompareNCAATourneySeason(int season, NCAATourneySeason t1TourneySeason, NCAATourneySeason t2TourneySeason)
@@ -128,40 +128,40 @@ namespace MarchMadness.Engines.Engine2018
         {
             HigherStatComparer(season, "Average NCAATourney Season Win Field Goals Ratio",
                 t1TourneySeason.AverageNCAATourneySeasonWinFieldGoalsMade / t1TourneySeason.AverageNCAATourneySeasonWinFieldGoalsAttempted,
-                t2TourneySeason.AverageNCAATourneySeasonWinFieldGoalsMade / t2TourneySeason.AverageNCAATourneySeasonWinFieldGoalsAttempted, 10);
+                t2TourneySeason.AverageNCAATourneySeasonWinFieldGoalsMade / t2TourneySeason.AverageNCAATourneySeasonWinFieldGoalsAttempted, 1);
             HigherStatComparer(season, "Average NCAATourney Season Win Three Pointers Ratio",
                 t1TourneySeason.AverageNCAATourneySeasonWinThreePointersMade / t1TourneySeason.AverageNCAATourneySeasonWinThreePointersAttempted,
-                t2TourneySeason.AverageNCAATourneySeasonWinThreePointersMade / t2TourneySeason.AverageNCAATourneySeasonWinThreePointersAttempted, 10);
+                t2TourneySeason.AverageNCAATourneySeasonWinThreePointersMade / t2TourneySeason.AverageNCAATourneySeasonWinThreePointersAttempted, 1);
             HigherStatComparer(season, "Average NCAATourney Season Win Free Throws Ratio",
                 t1TourneySeason.AverageNCAATourneySeasonWinFreeThrowsMade / t1TourneySeason.AverageNCAATourneySeasonWinFreeThrowsAttempted,
-                t2TourneySeason.AverageNCAATourneySeasonWinFreeThrowsMade / t2TourneySeason.AverageNCAATourneySeasonWinFreeThrowsAttempted, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Win Offensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonWinOffensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonWinOffensiveRebounds, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Win Defensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonWinDefensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonWinDefensiveRebounds, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Win Assists", t1TourneySeason.AverageNCAATourneySeasonWinAssists, t2TourneySeason.AverageNCAATourneySeasonWinAssists, 10);
-            LowerStatComparer(season, "Average NCAATourney Season Win Turnovers", t1TourneySeason.AverageNCAATourneySeasonWinTurnoversCommitted, t2TourneySeason.AverageNCAATourneySeasonWinTurnoversCommitted, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Win Steals", t1TourneySeason.AverageNCAATourneySeasonWinSteals, t2TourneySeason.AverageNCAATourneySeasonWinSteals, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Win Blocks", t1TourneySeason.AverageNCAATourneySeasonWinBlocks, t2TourneySeason.AverageNCAATourneySeasonWinBlocks, 10);
-            LowerStatComparer(season, "Average NCAATourney Season Win Fouls", t1TourneySeason.AverageNCAATourneySeasonWinPersonalFoulsCommitted, t2TourneySeason.AverageNCAATourneySeasonWinPersonalFoulsCommitted, 10);
+                t2TourneySeason.AverageNCAATourneySeasonWinFreeThrowsMade / t2TourneySeason.AverageNCAATourneySeasonWinFreeThrowsAttempted, 1);
+            HigherStatComparer(season, "Average NCAATourney Season Win Offensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonWinOffensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonWinOffensiveRebounds, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Win Defensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonWinDefensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonWinDefensiveRebounds, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Win Assists", t1TourneySeason.AverageNCAATourneySeasonWinAssists, t2TourneySeason.AverageNCAATourneySeasonWinAssists, 5);
+            LowerStatComparer(season, "Average NCAATourney Season Win Turnovers", t1TourneySeason.AverageNCAATourneySeasonWinTurnoversCommitted, t2TourneySeason.AverageNCAATourneySeasonWinTurnoversCommitted, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Win Steals", t1TourneySeason.AverageNCAATourneySeasonWinSteals, t2TourneySeason.AverageNCAATourneySeasonWinSteals, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Win Blocks", t1TourneySeason.AverageNCAATourneySeasonWinBlocks, t2TourneySeason.AverageNCAATourneySeasonWinBlocks, 5);
+            LowerStatComparer(season, "Average NCAATourney Season Win Fouls", t1TourneySeason.AverageNCAATourneySeasonWinPersonalFoulsCommitted, t2TourneySeason.AverageNCAATourneySeasonWinPersonalFoulsCommitted, 5);
         }
 
         private void CompareTourneySeasonLoseStats(int season, NCAATourneySeason t1TourneySeason, NCAATourneySeason t2TourneySeason)
         {
             HigherStatComparer(season, "Average NCAATourney Season Lose Field Goals Ratio",
                 t1TourneySeason.AverageNCAATourneySeasonLoseFieldGoalsMade / t1TourneySeason.AverageNCAATourneySeasonLoseFieldGoalsAttempted,
-                t2TourneySeason.AverageNCAATourneySeasonLoseFieldGoalsMade / t2TourneySeason.AverageNCAATourneySeasonLoseFieldGoalsAttempted, 10);
+                t2TourneySeason.AverageNCAATourneySeasonLoseFieldGoalsMade / t2TourneySeason.AverageNCAATourneySeasonLoseFieldGoalsAttempted, 1);
             HigherStatComparer(season, "Average NCAATourney Season Lose Three Pointers Ratio",
                 t1TourneySeason.AverageNCAATourneySeasonLoseThreePointersMade / t1TourneySeason.AverageNCAATourneySeasonLoseThreePointersAttempted,
-                t2TourneySeason.AverageNCAATourneySeasonLoseThreePointersMade / t2TourneySeason.AverageNCAATourneySeasonLoseThreePointersAttempted, 10);
+                t2TourneySeason.AverageNCAATourneySeasonLoseThreePointersMade / t2TourneySeason.AverageNCAATourneySeasonLoseThreePointersAttempted, 1);
             HigherStatComparer(season, "Average NCAATourney Season Lose Free Throws Ratio",
                 t1TourneySeason.AverageNCAATourneySeasonLoseFreeThrowsMade / t1TourneySeason.AverageNCAATourneySeasonLoseFreeThrowsAttempted,
-                t2TourneySeason.AverageNCAATourneySeasonLoseFreeThrowsMade / t2TourneySeason.AverageNCAATourneySeasonLoseFreeThrowsAttempted, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Lose Offensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonLoseOffensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonLoseOffensiveRebounds, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Lose Defensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonLoseDefensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonLoseDefensiveRebounds, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Lose Assists", t1TourneySeason.AverageNCAATourneySeasonLoseAssists, t2TourneySeason.AverageNCAATourneySeasonLoseAssists, 10);
-            LowerStatComparer(season, "Average NCAATourney Season Lose Turnovers", t1TourneySeason.AverageNCAATourneySeasonLoseTurnoversCommitted, t2TourneySeason.AverageNCAATourneySeasonLoseTurnoversCommitted, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Lose Steals", t1TourneySeason.AverageNCAATourneySeasonLoseSteals, t2TourneySeason.AverageNCAATourneySeasonLoseSteals, 10);
-            HigherStatComparer(season, "Average NCAATourney Season Lose Blocks", t1TourneySeason.AverageNCAATourneySeasonLoseBlocks, t2TourneySeason.AverageNCAATourneySeasonLoseBlocks, 10);
-            LowerStatComparer(season, "Average NCAATourney Season Lose Fouls", t1TourneySeason.AverageNCAATourneySeasonLosePersonalFoulsCommitted, t2TourneySeason.AverageNCAATourneySeasonLosePersonalFoulsCommitted, 10);
+                t2TourneySeason.AverageNCAATourneySeasonLoseFreeThrowsMade / t2TourneySeason.AverageNCAATourneySeasonLoseFreeThrowsAttempted, 1);
+            HigherStatComparer(season, "Average NCAATourney Season Lose Offensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonLoseOffensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonLoseOffensiveRebounds, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Lose Defensive Rebounds", t1TourneySeason.AverageNCAATourneySeasonLoseDefensiveRebounds, t2TourneySeason.AverageNCAATourneySeasonLoseDefensiveRebounds, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Lose Assists", t1TourneySeason.AverageNCAATourneySeasonLoseAssists, t2TourneySeason.AverageNCAATourneySeasonLoseAssists, 5);
+            LowerStatComparer(season, "Average NCAATourney Season Lose Turnovers", t1TourneySeason.AverageNCAATourneySeasonLoseTurnoversCommitted, t2TourneySeason.AverageNCAATourneySeasonLoseTurnoversCommitted, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Lose Steals", t1TourneySeason.AverageNCAATourneySeasonLoseSteals, t2TourneySeason.AverageNCAATourneySeasonLoseSteals, 5);
+            HigherStatComparer(season, "Average NCAATourney Season Lose Blocks", t1TourneySeason.AverageNCAATourneySeasonLoseBlocks, t2TourneySeason.AverageNCAATourneySeasonLoseBlocks, 5);
+            LowerStatComparer(season, "Average NCAATourney Season Lose Fouls", t1TourneySeason.AverageNCAATourneySeasonLosePersonalFoulsCommitted, t2TourneySeason.AverageNCAATourneySeasonLosePersonalFoulsCommitted, 5);
         }
 
         private void LowerStatComparer(int season, string category, double t1Value, double t2Value, double spread = 0)
