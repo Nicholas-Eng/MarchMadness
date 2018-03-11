96c4bbc9d59b32e428c104414015d14e59009948
Nick <nicholas.eng0218@gmail.com>
2018-03-10 20:00:30 -0500

    Modify ReadMe and add exe
    Modify ReadMe and add exe

--- CSVParser.cs
+++ CSVParser.cs
@@ -7,63 +7,93 @@ namespace MarchMadness
 {
     public static class CSVParser
     {
-        public static IEnumerable<T> ParseCsvFile<T>(string fileName) {
+        public static IEnumerable<T> ParseCsvFile<T>(string fileName)
+        {
+            var parsedData = new List<T>();
+            string filePath = GetDataFilePath(fileName);
 
-            fileName = Constants.DATA_FOLDER + Path.DirectorySeparatorChar + fileName;
+            if(filePath != null) 
+            {
+                try
+                {
+                    using (var streamReader = new StreamReader(filePath))
+                    {
+                        string line;
+                        string[] propertyNames = new string[]{};
+                        string[] row;
+                        bool isHeader = true;
+                        while (!string.IsNullOrWhiteSpace(line = streamReader.ReadLine()))
+                        {
+                            if(isHeader)
+                            {
+                                propertyNames = line.Split(',');
+                                isHeader = false;
+                            }
+                            else
+                            {
+                                row = line.Split(',');
 
+                                if(row.Length == propertyNames.Length)
+                                {
+                                    var instance = (T)Activator.CreateInstance(typeof(T));
+
+                                    for(int i = 0, j = row.Length; i < j; i++)
+                                    {
+                                        PropertyInfo propertyInfo = instance.GetType().GetProperty(propertyNames[i]);
+                                        propertyInfo.SetValue(instance, Convert.ChangeType(row[i], propertyInfo.PropertyType), null);
+                                    }
+
+                                    parsedData.Add(instance);
+                                }
+                                else
+                                {
+                                    Logger.Error("CSV data invalid. File: " + fileName);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("Failed to parse CSV file:" + fileName);
+                    Logger.Error(ex.Message);
+                }
+            }
+
+            return parsedData;
+        }
+
+        private static string GetDataFilePath(string fileName)
+        {
             if(!fileName.EndsWith(".csv"))
             {
                 fileName += ".csv";
             }
 
-            var parsedData = new List<T>();
+            // Directly in VS Code
+            string path1 = Path.GetFullPath(Constants.DATA_FOLDER + Path.DirectorySeparatorChar + fileName);
+            // Debug bin
+            string path2 = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\" + Constants.DATA_FOLDER + Path.DirectorySeparatorChar + fileName);
+            // Exe
+            string path3 = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\..\\" + Constants.DATA_FOLDER + Path.DirectorySeparatorChar + fileName);
 
-            try
+            if(File.Exists(path1))
             {
-                using (var streamReader = new StreamReader(Path.GetFullPath(fileName)))
-                {
-                    string line;
-                    string[] propertyNames = new string[]{};
-                    string[] row;
-                    bool isHeader = true;
-                    while (!string.IsNullOrWhiteSpace(line = streamReader.ReadLine()))
-                    {
-                        if(isHeader)
-                        {
-                            propertyNames = line.Split(',');
-                            isHeader = false;
-                        }
-                        else
-                        {
-                            row = line.Split(',');
-
-                            if(row.Length == propertyNames.Length)
-                            {
-                                var instance = (T)Activator.CreateInstance(typeof(T));
-
-                                for(int i = 0, j = row.Length; i < j; i++)
-                                {
-                                    PropertyInfo propertyInfo = instance.GetType().GetProperty(propertyNames[i]);
-                                    propertyInfo.SetValue(instance, Convert.ChangeType(row[i], propertyInfo.PropertyType), null);
-                                }
-
-                                parsedData.Add(instance);
-                            }
-                            else
-                            {
-                                Logger.Error("CSV data invalid. File: " + fileName);
-                            }
-                        }
-                    }
-                }
+                return path1;
             }
-            catch (Exception ex)
+            else if(File.Exists(path2))
             {
-                Logger.Error("Failed to parse CSV file:" + fileName);
-                Logger.Error(ex.Message);
+                return path2;
             }
-
-            return parsedData;
+            else if(File.Exists(path3))
+            {
+                return path3;
+            }
+            else
+            {
+                Logger.Error("Failed to find data diretory path");
+                return null;
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,30 @@
 
 :basketball::basketball::basketball::basketball: March Madness 2018 :basketball::basketball::basketball::basketball:
 
+:basketball::basketball::basketball::basketball: Nicholas Eng :basketball::basketball::basketball::basketball:
+
 :basketball::basketball::basketball::basketball::basketball::basketball::basketball::basketball::basketball::basketball::basketball::basketball::basketball::basketball:
 
-By: Nicholas Eng
+## Description
+This is a console application built in C# and .NET Core.
+
+## How to Run Application
+There are two ways to run the application after pulling down the project.
+
+1. Run inside IDE.
+2. Run the executable: "MarchMadness\bin\Release\netcoreapp2.0\win10-x64\MarchMadness.exe".
+
+## How it Works
+1. User enters two teams.
+2. Data for both teams are read based off the seasons listed in the Constants.cs file.
+3. The retrieved data is stored and, in some cases, manipulated into objects.
+   - For example, collecting averages of data.
+4. A running point system is calculated for each category of data (i.e. Average Steals) for every season.
+   - The point system looks at which team had the better stat, then in some cases takes the difference divided by a pre-determined typical spread value.
+5. Once all the points for each category in every season are collected, the sum is then calculated for all points for each team in every season.
+6. Then, based off the number of seasons, the sum for that season is multiplied by the exponent of the season relative to this year.
+   - For example, if our totals for 2015 = 100, 2016 = 200, 2017 = 300 then our equations would look like:
+     (100 * e^(1)) + (200 * e^(2)) + (300 * e^(3)) = final score
+   - The exponent is used so that the more recent seasons are weighted heavier than older seasons.
+7. The summation of each value calculated in step 6 then gives us the team's final score.
+8. The team with the higher final score is deemed the winner.
